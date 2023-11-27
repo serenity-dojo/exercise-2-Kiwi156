cf093f251b727deddd1e282e967def96a26d0223
Katie Beattie <KatieBeattie13@gmail.com>
2023-11-27 16:34:20 +1300

    Completed exercise 2.

--- src/test/java/caffeinateme/OrderCoffeeSteps.java
+++ src/test/java/caffeinateme/OrderCoffeeSteps.java
@@ -4,6 +4,8 @@ import caffeinateme.model.CoffeeShop;
 import caffeinateme.model.Customer;
 import caffeinateme.model.Order;
 import caffeinateme.model.OrderStatus;
+import io.cucumber.java.ParameterType;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -13,26 +15,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderCoffeeSteps {
 
+
     Customer cathy = Customer.named("Cathy");
     CoffeeShop coffeeShop = new CoffeeShop();
     Order order;
-    @Given("^Cathy is (\\d*) metres from the coffee shop$")
-    public void cathy_is_metres_from_the_coffee_shop(Integer distanceInMeters) {
 
+    @Given("Cathy is {float} metre(s) from the coffee shop")
+    public void cathy_is_metres_from_the_coffee_shop(Float distanceInMeters) {
         cathy.setDistanceFromShop(distanceInMeters);
-    }
-    @When("^Cathy orders a(.*)$")
-    public void cathy_orders_a_(String orderedProduct) {
 
+    }
+
+    //   @When("^Cathy (?:orders|has ordered) a (.*)$")
+    // public void cathy_orders_a(String orderedProduct) {
+    //   this.order = Order.of(1, orderedProduct).forCustomer(cathy);
+    // cathy.placesAnOrderFor(order).at(coffeeShop);
+    // }
+    @When("Cathy orders a/an {string}")
+    public void cathy_orders_a(String orderedProduct) {
         this.order = Order.of(1, orderedProduct).forCustomer(cathy);
         cathy.placesAnOrderFor(order).at(coffeeShop);
     }
+
     @Then("Barry should receive the order")
     public void barry_should_receive_the_order() {
 
         Assertions.assertThat(coffeeShop.getPendingOrders()).contains(order);
     }
-    @Then("^Barry should know that the order is (.*)$")
+
+    @ParameterType(value="(Normal|High|Urgent)")
+    public OrderStatus orderStatus(String statusValue){
+        return OrderStatus.valueOf(statusValue);
+    }
+    @Then("Barry should know that the order is {orderStatus}")
     public void barry_should_know_that_the_order_is(OrderStatus expectedStatus)
     {
 
